daaa7105e4006110d1919c11c7e483533e8e2478
Vanya Mihova <vanya.mihova89@gmail.com>
2020-11-15 09:11:38 +0200

    Clean comments

--- gallery-manager/Database/Entity/ImageEntity.cs
+++ gallery-manager/Database/Entity/ImageEntity.cs
@@ -24,19 +24,3 @@ namespace gallery_manager.Database.Entity
 
     }
 }
-
-/*CREATE TABLE Gallery
-    (
-        Id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        Label varchar(255)
-    );
-
-
-CREATE TABLE Image
-    (
-        Id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        Label varchar(30),
-        Filename varchar(255),
-        GalleryId int,
-        FOREIGN KEY(GalleryId) REFERENCES Gallery(Id) ON DELETE CASCADE
-    );*/
